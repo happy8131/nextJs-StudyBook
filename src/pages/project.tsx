f4c063f5eb360c4a21f4b73d1dd5661f05e9ea23
happy8131 <lion8131@naver.com>
2023-09-26 17:12:31 +0900

    infinity Scroll

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,19 +1,61 @@
 import { Layout } from "../components/layout";
 import axios from "axios";
 import StudyItem from "../components/studys/studyItem";
+import { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+const lodash = require("lodash");
 
 const Projects = ({ studys }: any) => {
+  const [studyData, setStudyData] = useState(studys.results.slice(0, 10));
+  const [page, setPage] = useState(10);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const len = studys.results.length - page;
+
+    const handleScroll = async () => {
+      // eslint-disable-next-line sonarjs/no-collapsible-if
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 100
+      ) {
+        if (len > 0) {
+          setLoading(true);
+          const copy = lodash.cloneDeep(studys.results);
+          setStudyData(studyData.concat(copy.slice(page, page + 10)));
+          setPage(page + 10);
+          setLoading(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [page]);
+
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center min-h-screen px-5 mb-10 px-6">
         <h1 className="text-4xl font-bold sm:text-6xl">
-          개발자 스터디 : {studys?.results.length}
+          개발자 스터디 : {studyData.length}
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 py-10 m-6 gap-8">
-          {studys.results.map((item: any) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8">
+          {studyData.map((item: any) => (
             <StudyItem key={item.id} data={item} />
           ))}
         </div>
+        {loading && (
+          <div className="h-full flex justify-center text-xl font-bold">
+            <AiOutlineLoading3Quarters
+              className="rounded-t-lg mt-9 ml-3 animate-spin"
+              size={30}
+            />
+          </div>
+        )}
       </div>
     </Layout>
   );
@@ -36,8 +78,6 @@ export const getStaticProps = async () => {
     .request(options)
     .then(function (response) {
       const resData = response.data;
-      // const studyId = resData.results.map((item) => item.id);
-
       studys = resData;
     })
     .catch(function (error) {
