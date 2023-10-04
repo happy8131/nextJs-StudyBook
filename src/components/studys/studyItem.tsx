6f1ec119f9c0686c6eff499d02c3b8692a9002c4
happy8131 <lion8131@naver.com>
2023-10-04 14:36:05 +0900

    branch

--- src/components/studys/studyItem.tsx
+++ src/components/studys/studyItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const StudyItem = ({ data }: any) => {
   const studyTitle = data.properties?.name.title[0].plain_text;
-  const blogLink = data.properties?.blog.rich_text[0].plain_text;
+  const blogLink = data.properties.blog.rich_text[0].plain_text;
   const description = data.properties.description.rich_text[0].plain_text;
   const tag = data.properties?.tag.multi_select;
   const imgSrc = data.cover.file.url || data.cover.external.url;
